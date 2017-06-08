7387a925b65e4e2186a8ee68a7096487c0f35c72
AngusG <premiumdriedfoods@gmail.com>
2017-06-08 14:40:51 -0400

    tidy up data loader and normalization

--- lstm.py
+++ lstm.py
@@ -7,42 +7,56 @@ from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Hide messy TensorFlow warnings
-warnings.filterwarnings("ignore") #Hide messy Numpy warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Hide messy TensorFlow warnings
+warnings.filterwarnings("ignore")  # Hide messy Numpy warnings
 
-def load_data(filename, seq_len, normalise_window):
-    f = open(filename, 'rb').read()
-    data = f.decode().split('\n')
 
-    sequence_length = seq_len + 1
-    result = []
-    for index in range(len(data) - sequence_length):
-        result.append(data[index: index + sequence_length])
-    
+def load_data(filename, seq_len, train_ratio, normalise_window):
+
+    with open(filename, 'r') as f:
+        data = f.read().split('\n')
+    data = np.array(data).astype('float32')
+
+    ''' the reason for this is so that when we 
+    peel off the last data point as a label 
+    below (y_train = ...), we still end up 
+    with a nice even number, e.g 50 '''
+    seq_len += 1
+    result = np.zeros((len(data), seq_len, 1))
+
+    # this shouldn't affect the sine wave data
     if normalise_window:
-        result = normalise_windows(result)
+        data -= data.mean()
+        data /= data.max()
 
-    result = np.array(result)
+    for i in range(len(data) - seq_len):
+        result[i, :, 0] = data[i:i + seq_len]
 
-    row = round(0.9 * result.shape[0])
-    train = result[:int(row), :]
-    np.random.shuffle(train)
-    x_train = train[:, :-1]
-    y_train = train[:, -1]
-    x_test = result[int(row):, :-1]
-    y_test = result[int(row):, -1]
+    # create training and testing split
+    split = int(round(train_ratio * len(data)))
+    train = result[:split, :, :]
+    test = result[split:, :, :]
 
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))  
+    # np.random.shuffle(train)
+
+    # take all but the last data point of each window as input
+    x_train = train[:, :-1, :]
+    # take the last data point of each window as label
+    y_train = train[:, -1, :]
+    # same procedure for test
+    x_test = test[:, :-1, :]
+    y_test = test[:, -1, :]
 
     return [x_train, y_train, x_test, y_test]
-
+'''
 def normalise_windows(window_data):
     normalised_data = []
     for window in window_data:
-        normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+        normalised_window = [((float(p) / float(window[0])) - 1)
+                             for p in window]
         normalised_data.append(normalised_window)
     return normalised_data
+'''
 
 def build_model(layers):
     model = Sequential()
@@ -67,31 +81,39 @@ def build_model(layers):
     print("> Compilation Time : ", time.time() - start)
     return model
 
+
 def predict_point_by_point(model, data):
-    #Predict each timestep given the last sequence of true data, in effect only predicting 1 step ahead each time
+    # Predict each timestep given the last sequence of true data, in effect
+    # only predicting 1 step ahead each time
     predicted = model.predict(data)
     predicted = np.reshape(predicted, (predicted.size,))
     return predicted
 
+
 def predict_sequence_full(model, data, window_size):
-    #Shift the window by 1 new prediction each time, re-run predictions on new window
+    # Shift the window by 1 new prediction each time, re-run predictions on
+    # new window
     curr_frame = data[0]
     predicted = []
     for i in range(len(data)):
-        predicted.append(model.predict(curr_frame[newaxis,:,:])[0,0])
+        predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
         curr_frame = curr_frame[1:]
-        curr_frame = np.insert(curr_frame, [window_size-1], predicted[-1], axis=0)
+        curr_frame = np.insert(
+            curr_frame, [window_size - 1], predicted[-1], axis=0)
     return predicted
 
+
 def predict_sequences_multiple(model, data, window_size, prediction_len):
-    #Predict sequence of 50 steps before shifting prediction run forward by 50 steps
+    # Predict sequence of 50 steps before shifting prediction run forward by
+    # 50 steps
     prediction_seqs = []
-    for i in range(int(len(data)/prediction_len)):
-        curr_frame = data[i*prediction_len]
+    for i in range(int(len(data) / prediction_len)):
+        curr_frame = data[i * prediction_len]
         predicted = []
         for j in range(prediction_len):
-            predicted.append(model.predict(curr_frame[newaxis,:,:])[0,0])
+            predicted.append(model.predict(curr_frame[newaxis, :, :])[0, 0])
             curr_frame = curr_frame[1:]
-            curr_frame = np.insert(curr_frame, [window_size-1], predicted[-1], axis=0)
+            curr_frame = np.insert(
+                curr_frame, [window_size - 1], predicted[-1], axis=0)
         prediction_seqs.append(predicted)
     return prediction_seqs
