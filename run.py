12f12b550c0e40f6dabc04db1e108dbf4d892ff3
AngusG <premiumdriedfoods@gmail.com>
2017-06-08 14:36:26 -0400

    add some defensive checks for filename

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import lstm
-import time
+import os, sys, time
 import argparse
 import matplotlib.pyplot as plt
 
@@ -27,31 +27,35 @@ if __name__=='__main__':
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--csv', help='either sp500.csv or sinwave.csv', default='sinwave.csv')
+	parser.add_argument('--split', help='ratio of training to testing data', type=float, default=0.9)
 	args = parser.parse_args()
 
 	global_start_time = time.time()
 	epochs  = 1
 	seq_len = 50
 
-	print('> Loading data... ')
+	if os.path.exists(args.csv):
+		print('> Loading %s' % args.csv)
+	else:
+		print('> Error, %s does not exist' % args.csv)
+		sys.exit(1)
 
 	data_loader_start_time = time.time()
 
 	if args.csv == 'sp500.csv':
-		X_train, y_train, X_test, y_test = lstm.load_data(args.csv, seq_len, True)
+		X_train, y_train, X_test, y_test = lstm.load_data(args.csv, seq_len, args.split, True)
 	else:
-		X_train, y_train, X_test, y_test = lstm.load_data(args.csv, seq_len, False)
+		X_train, y_train, X_test, y_test = lstm.load_data(args.csv, seq_len, args.split, False)
 
 	print("> Data Loading Time : ", time.time() - data_loader_start_time)
 
 	model = lstm.build_model([1, 50, 100, 1])
-
 	model.fit(
 	    X_train,
 	    y_train,
 	    batch_size=512,
 	    nb_epoch=epochs,
-	    validation_split=0.05)
+	    validation_data=(X_test, y_test))
 	
 	print('Training duration (s) : ', time.time() - global_start_time)
 
@@ -60,7 +64,4 @@ if __name__=='__main__':
 
 	#predicted = lstm.predict_sequence_full(model, X_test, seq_len)
 	predicted = lstm.predict_point_by_point(model, X_test)        
-	plot_results(predicted, y_test)  
-
-	
-	
+	plot_results(predicted, y_test)
